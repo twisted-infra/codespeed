1c4bd383fb5269fd82da25137c6e2f05e411b641
Tom Prince <tom.prince@ualberta.net>
2013-05-13 09:58:23 -0600

    Initial commit.

---
+++ fabfile.py
@@ -0,0 +1,49 @@
+"""
+Support for benchmark reporting.
+"""
+
+from fabric.api import run, settings
+
+from braid import git, cron, pip
+from braid.twisted import service
+from braid.tasks import addTasks
+
+from braid import config
+__all__ = [ 'config' ]
+
+
+class Codespeed(service.Service):
+
+    def task_install(self):
+        """
+        Install codespeed, a benchmark reporting tool
+        """
+        # Bootstrap a new service environment
+        self.bootstrap()
+
+        with settings(user=self.serviceUser):
+            run('/bin/ln -nsf {}/start {}/start'.format(self.configDir, self.binDir))
+            pip.install('django')
+            self.update()
+            cron.install(self.serviceUser, '{}/crontab'.format(self.configDir))
+
+
+    def update(self):
+        """
+        Update config.
+        """
+        with settings(user=self.serviceUser):
+            git.branch('https://github.com/twisted-infra/codespeed', self.configDir)
+            git.branch('https://github.com/twisted-infra/codespeed-source', '~/codespeed')
+
+
+    def task_update(self):
+        """
+        Update config and restart.
+        """
+        self.update()
+        self.task_restart()
+
+
+
+addTasks(globals(), Codespeed('codespeed').getTasks())
