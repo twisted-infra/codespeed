ba2277868afedf5262fb9648b031d1e8e835801c
Tom Prince <tom.prince@ualberta.net>
2013-05-14 17:14:36 -0600

    Set timezone.

--- local_settings.py
+++ local_settings.py
@@ -7,3 +7,5 @@ TEMPLATE_DIRS = ( sibpath(__file__, 'templates'), ) + TEMPLATE_DIRS
 
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = os.path.expanduser("~/data/codespeed.db")
+
+TIME_ZONE = 'America/New_York'
