d7559f70c229068bda875233d91388f17dd9fc61
Tom Prince <tom.prince@ualberta.net>
2013-05-14 18:01:21 -0600

    Create proper data directory.

--- fabfile.py
+++ fabfile.py
@@ -25,7 +25,7 @@ class Codespeed(service.Service):
 
         with settings(user=self.serviceUser):
             run('/bin/ln -nsf {}/start {}/start'.format(self.configDir, self.binDir))
-            run('mkdir -p data')
+            run('mkdir -p ~/data')
             pip.install('Django==1.2.7')
             self.update()
             cron.install(self.serviceUser, '{}/crontab'.format(self.configDir))
