31c3b049cb37d24d67fff545f6bdc311942b35af
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-16 21:40:23 -0400

    Import the secret key from the generated file

--- local_settings.py
+++ local_settings.py
@@ -3,6 +3,10 @@ from speedcenter.settings import *
 import os
 from twisted.python.util import sibpath
 
+from . import secret_key
+
+SECRET_KEY = secret_key.SECRET_KEY
+
 TEMPLATE_DIRS = ( sibpath(__file__, 'templates'), ) + TEMPLATE_DIRS
 
 DATABASE_ENGINE = 'sqlite3'
